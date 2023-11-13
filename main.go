70795787670ea5a9265b8ec4e8ec64b1141df2e9
Hossein samiei <syhsamiei@gmail.com>
2023-11-13 16:08:11 +0330

    everything before user imput

--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import "fmt"
 
 // where to start ? main function 
 func main(){
-	var ConferenceName = "Go Conference"
-	const ConferenceTickets = 50
+	var ConferenceName string = "Go Conference"
+	const ConferenceTickets int = 50
+	var remainingTickets int = 50
 
-	fmt.Println("Welcome to the Nexaloak", ConferenceName ,"Society")
+
+	fmt.Printf("We can use %T for show the type of var, for example in here I want to show the type of Conference Ticket", ConferenceTickets)
+
+
+	// fmt.Println("Welcome to the Nexaloak", ConferenceName ,"Society")
+	fmt.Printf("Welcome to the Nexaloak %v Society", ConferenceName)
 	fmt.Println("Here you can book one node in Nexaloak Blockchain")
- 
+	// fmt.Println("There are ", ConferenceTickets, "Tickets and ", remainingTickets, "are still available")
+	fmt.Printf("There are %v Tickets and %v  are still available", ConferenceTickets, remainingTickets)
 
-	// var ConferenceName = "Go Conference"
-	// fmt.Print(ConferenceName)
+
+	var userName string
+	var userTicket int
+
+	fmt.Println(userTicket)
+	// ask user for name 
+
+	userName = "Tom"
+	fmt.Println(userName)
 
 
 
