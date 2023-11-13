8f882cb113a5d05467507aa21e4eaeff9590c32a
Hossein samiei <syhsamiei@gmail.com>
2023-11-13 15:55:27 +0330

    feat: easy peasy syntax

--- main.go
+++ main.go
@@ -4,5 +4,17 @@ import "fmt"
 
 // where to start ? main function 
 func main(){
-	fmt.Println("Hello world")
+	var ConferenceName = "Go Conference"
+	const ConferenceTickets = 50
+
+	fmt.Println("Welcome to the Nexaloak", ConferenceName ,"Society")
+	fmt.Println("Here you can book one node in Nexaloak Blockchain")
+ 
+
+	// var ConferenceName = "Go Conference"
+	// fmt.Print(ConferenceName)
+
+
+
+
 }
